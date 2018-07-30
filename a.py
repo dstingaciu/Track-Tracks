aff7a1cc0fcdca81ed4197916f816fcfe77dda35
Dan Stingaciu <stingaciu.sebastian@gmail.com>
2018-07-30 15:02:17 -0400

    Pycharm doesnt work well with some web based APIs
    So I got rid of it

--- a.py
+++ a.py
@@ -1,8 +1,11 @@
 import spotipy
 from spotipy import util
 
-username = input("Enter your Spotify Username: ")
+username = "zap12341"
 client_id = "da6e7e0d474a47b797943c65bfaa94fb"
 client_S = '3a5ad8a8a63d4733b56bfb638c2fc8d8'
+redUrl = 'https://dstingaciu.github.io/Track-Tracks/'
 
-util.prompt_for_user_token(username,scope=None,client_id,client_S, )
+token = util.prompt_for_user_token(username,'user-read-private user-read-email',client_id,client_S,redUrl)
+
+print ("Done")
--- venv/Lib/site-packages/spotipy_tui/__init__.py
+++
@@ -1,4 +0,0 @@
-import sys
-import os
-
-sys.path.insert(1, os.path.dirname(__file__))
