a708c57194c47943ae27469ebdbfd8c426c3c8d4
Dan Stingaciu <stingaciu.sebastian@gmail.com>
2018-08-02 02:45:31 -0400

    Fixed authorization for Spotify

--- a.py
+++ a.py
@@ -1,4 +1,7 @@
 import spotipy
+import responses
+import requests
+
 from spotipy import util
 
 username = "zap12341"
@@ -6,6 +9,13 @@ client_id = "da6e7e0d474a47b797943c65bfaa94fb"
 client_S = '3a5ad8a8a63d4733b56bfb638c2fc8d8'
 redUrl = 'https://dstingaciu.github.io/Track-Tracks/'
 
-token = util.prompt_for_user_token(username,'user-read-private user-read-email',client_id,client_S,redUrl)
+token = util.prompt_for_user_token(username,'user-read-currently-playing',client_id,client_S,redUrl)
 
-print ("Done")
+if token:
+	sp = spotipy.Spotify(auth = token)
+	cp = sp.currently_playing(market = "CA")
+	content = cp.get('item')
+	if(cp["is_playing"] == False):
+		print ("You are currently not playing anything")
+	else:
+		print ("You are currently playing "+content["name"]+" by "+ content["artists"][0]["name"])
